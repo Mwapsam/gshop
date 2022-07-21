9da5a33b4d37df38b96cd715630122f7b6cc5b3b
Mwapsam <mwapsam@gmail.com>
2022-07-21 14:07:43 +0200

    Create a shopping cart from user session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    protect_from_forgery prepend: true
+  
+    before_action :initialize_session
+    before_action :load_cart
+  
+    private
+  
+    def initialize_session
+      session[:cart] ||= [] # empty cart = empty array
+    end
+  
+    def load_cart
+      @cart = Product.find(session[:cart])
+    end
 end
