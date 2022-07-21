138337d2bf24446d957b6b173b36c5800efb6bd4
Mwapsam <mwapsam@gmail.com>
2022-07-21 14:10:21 +0200

    Create checkout controller

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,29 @@
+class CheckoutController < ApplicationController
+  def create
+    @session = Stripe::Checkout::Session.create({
+                                                  customer: current_user.stripe_customer_id,
+                                                  payment_method_types: ['card'],
+                                                  line_items: @cart.collect { |item| item.to_builder.attributes! },
+                                                  allow_promotion_codes: true,
+                                                  mode: 'payment',
+                                                  success_url: "#{success_url}?session_id={CHECKOUT_SESSION_ID}",
+                                                  cancel_url:
+                                                })
+    redirect_to @session.url, status: 303, allow_other_host: true
+  end
+
+  def success
+    if params[:session_id].present?
+      # session.delete(:cart)
+      session[:cart] = [] # empty cart = empty array
+      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ['line_items'] })
+      @session_with_expand.line_items.data.each do |line_item|
+        Product.find_by(stripe_product_id: line_item.price.product)
+      end
+    else
+      redirect_to cancel_url, alert: 'No info to display'
+    end
+  end
+
+  def cancel; end
+end
---
+++ test/decorators/checkout_decorator_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class CheckoutDecoratorTest < Draper::TestCase
+end
