829627e3cd511dc59304b60b36de46eca61cbd39
Mwapsam <mwapsam@gmail.com>
2022-07-22 11:02:14 +0200

    Customize add product form in activeadmin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,7 +1,18 @@
 ActiveAdmin.register Product do
   permit_params do
-    permitted = [:sku, :name, :price, :weight, :description, :category, :stock, :mage, :thumbnails]
-    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted = [:sku, :name, :price, :weight, :description, :category, :stock, :image, :thumbnails]
+    permitted << :other if params[:action] == 'create' && current_admin_user
     permitted
+  end
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      input :image , as: :file, hint: f.object.image.present? ? image_tag(f.object.image.variant(resize: '200x200')) : content_tag(:span, "No image yet")
+      input :thumbnails, as: :file, multiple: true, hint: f.object.thumbnails.map do |thumbnail|
+        image_tag(thumbnail.variant(resize: '200x200'))
+      end
+    end
+    f.actions
   end
 end
