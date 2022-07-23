db698be4042d0289bd96c9221728280fe781bf7e
Mwapsam <mwapsam@gmail.com>
2022-07-23 08:51:48 +0200

    Create cart tooltip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+    const btn = document.getElementById("btn");
+    const cart = document.getElementById("cart");
+
+    btn.addEventListener("mouseenter", () => {
+        cart.classList.remove("hidden");
+        console.log("mouseenter");
+    })
+    btn.addEventListener("click", () => {
+        cart.classList.add("hidden");
+    })
