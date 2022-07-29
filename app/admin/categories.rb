92812028eb280352a6f453f3dc5a7f9c7a5b80be
Mwapsam <mwapsam@gmail.com>
2022-07-29 14:18:57 +0200

    Add category index

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,16 +1,11 @@
 ActiveAdmin.register Category do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :name, :description, :image
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      input :image , as: :file, hint: f.object.image.present? ? image_tag(f.object.image.variant(resize: '200x200')) : content_tag(:span, "No image yet")
+    end
+    f.actions
+  end
 end
