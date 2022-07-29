3fa9d69ed44c4b82f6cca8f398b58e78b56b07b5
Mwapsam <mwapsam@gmail.com>
2022-07-29 14:19:18 +0200

    Customise the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,31 +4,26 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    div class: 'blank_slate_container', id: 'dashboard_default_message' do
-      span class: 'blank_slate' do
-        span I18n.t('active_admin.dashboard_welcome.welcome')
-        small I18n.t('active_admin.dashboard_welcome.call_to_action')
+    columns do
+      column do 
+        panel "Recent Products" do
+          table_for Product.order('created_at desc').limit(5) do
+            column :name
+            column :sku
+            column :price do |product|
+              number_to_currency product.price_cents
+            end
+            column :stock
+          end
+        end
+      end
+      column do
+        panel "Categories" do
+          table_for Category.order('created_at desc').limit(5) do
+            columns :name, :description
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end
 end
