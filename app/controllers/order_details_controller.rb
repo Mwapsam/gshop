3f9da63c55dced78823bcc179157a89f9cc1e752
Mwapsam <mwapsam@gmail.com>
2022-08-01 16:32:17 +0200

    Add OrderDetail methods

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -1,2 +1,18 @@
 class OrderDetailsController < ApplicationController
+    def new
+        @order_detail = OrderDetail.new
+        @product = Product.find(params[:id])
+        @order = Order.find(params[:id])
+    end
+    
+    def create
+        @order_detail = OrderDetail.new(order_detail_params)
+        @order_detail.save
+    end
+
+    private
+
+    def order_detail_params
+        params.permit(:order_id, :product_id, :quantity)
+    end
 end
