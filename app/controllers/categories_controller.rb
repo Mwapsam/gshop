6306021e7d0ee529ba9cb2ded9e86b92a0db46a9
Mwapsam <mwapsam@gmail.com>
2022-08-01 18:09:58 +0200

    Add category show method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,6 @@ class CategoriesController < ApplicationController
 
     def show
         @products = Product.where(category_id: params[:id])
+        @category = Category.find(params[:id])
     end
 end
