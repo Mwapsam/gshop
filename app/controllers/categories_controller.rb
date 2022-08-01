a51257e5e7cac34b9f4e9ef1a65e8944558fc154
Mwapsam <mwapsam@gmail.com>
2022-08-01 17:56:20 +0200

    Add styles to category component

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,8 @@ class CategoriesController < ApplicationController
     def index
         @category = Category.all
     end
+
+    def show
+        @products = Product.where(category_id: params[:id])
+    end
 end
