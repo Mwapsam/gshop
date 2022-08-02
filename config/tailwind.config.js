1cb8e303377de43ed2ee2d43b8a81726d9aecbd7
Mwapsam <mwapsam@gmail.com>
2022-08-02 19:41:01 +0200

    Revert to Attach s3 bucket

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,13 +12,6 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      screens: {
-        'sm': {'min': '640px', 'max': '767px'},
-        'md': {'min': '768px', 'max': '1023px'}, 
-        'lg': {'min': '1024px', 'max': '1279px'},
-        'xl': {'min': '1280px', 'max': '1535px'},
-        '2xl': {'min': '1536px'},
-      },
     },
   },
   plugins: [
